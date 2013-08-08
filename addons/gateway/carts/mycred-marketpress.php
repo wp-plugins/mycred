96572d555c2cf50b6f97b7a1b03793e2b1022d1a
designbymerovingi <designbymerovingi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-08 12:29:20 +0000

    Added Template for "Insufficient Funds" and for "Visitors". Added table to checkout page to show users current balance, cost in points and balance after purchase.
    git-svn-id: https://plugins.svn.wordpress.org/mycred/trunk@753482 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- addons/gateway/carts/mycred-marketpress.php
+++ addons/gateway/carts/mycred-marketpress.php
@@ -50,6 +50,7 @@ if ( !class_exists( 'MP_Gateway_myCRED' ) ) {
 			$this->public_name = ( !empty( $settings['gateways']['mycred']['name'] ) ) ? $settings['gateways']['mycred']['name'] : apply_filters( 'mycred_label', myCRED_NAME );
 			$this->method_img_url = plugins_url( 'assets/images/cred-icon32.png', myCRED_THIS );
 			$this->method_button_img_url = $settings['gateways']['mycred']['name'];
+			$this->mycred = mycred_get_settings();
 		}
 		
 		/**
@@ -67,77 +68,12 @@ if ( !class_exists( 'MP_Gateway_myCRED' ) ) {
 		}
 
 		/**
-		 * Return fields you need to add to the payment screen, like your credit card info fields
-		 *
-		 * @param array $cart. Contains the cart contents for the current blog, global cart if $mp->global_cart is true
-		 * @param array $shipping_info. Contains shipping info and email in case you need it
-		 * @since 1.1
+		 * Returns the current carts total.
+		 * @since 1.2
 		 * @version 1.0
 		 */
-		function payment_form( $cart, $shipping_info ) {
+		function get_cart_total( $cart = NULL ) {
 			global $mp;
-			
-			$settings = get_option( 'mp_settings' );
-			$mycred = mycred_get_settings();
-			return '<div id="mp-mycred-balance">' . $mycred->template_tags_general( $settings['gateways']['mycred']['name'] ) . '</div>';
-		}
-
-		/**
-		 * Return the chosen payment details here for final confirmation. You probably don't need
-		 * to post anything in the form as it should be in your $_SESSION var already.
-		 *
-		 * @param array $cart. Contains the cart contents for the current blog, global cart if $mp->global_cart is true
-		 * @param array $shipping_info. Contains shipping info and email in case you need it
-		 * @since 1.1
-		 * @version 1.0
-		 */
-		function confirm_payment_form( $cart, $shipping_info ) {
-			global $mp;
-
-			$settings = get_option( 'mp_settings' );
-			$mycred = mycred_get_settings();
-			$user_id = get_current_user_id();
-			return '<div id="mp-mycred-balance">' . $mycred->template_tags_user( $settings['gateways']['mycred']['instructions'], $user_id ) . '</div>';
-		}
-		
-		function process_payment_form( $cart, $shipping_info ) { }
-
-		/**
-		 * Use this to do the final payment. Create the order then process the payment. If
-		 * you know the payment is successful right away go ahead and change the order status
-		 * as well.
-		 * Call $mp->cart_checkout_error($msg, $context); to handle errors. If no errors
-		 * it will redirect to the next step.
-		 *
-		 * @param array $cart. Contains the cart contents for the current blog, global cart if $mp->global_cart is true
-		 * @param array $shipping_info. Contains shipping info and email in case you need it
-		 * @since 1.1
-		 * @version 1.1
-		 */
-		function process_payment( $cart, $shipping_info ) {
-			global $mp;
-			
-			$settings = get_option('mp_settings');
-			$timestamp = time();
-
-			// This gateway requires buyer to be logged in
-			if ( !is_user_logged_in() )
-				$mp->cart_checkout_error(
-					sprintf(
-						__( 'Sorry, but you must be logged in to use this gateway. Please <a href="%s">Login</a> or <a href="%s">select a different payment method</a>.', 'mycred' ),
-						wp_login_url( mp_checkout_step_url( 'checkout' ) ),
-						mp_checkout_step_url( 'checkout' )
-					)
-				);
-			
-			$mycred = mycred_get_settings();
-			$user_id = get_current_user_id();
-			
-			// Make sure current user is not excluded from using myCRED
-			if ( $mycred->exclude_user( $user_id ) )
-				$mp->cart_checkout_error(
-					sprintf( __( 'Sorry, but you can not use this gateway as your account is excluded. Please <a href="%s">select a different payment method</a>.', 'mycred' ), mp_checkout_step_url( 'checkout' ) )
-				);
 
 			// Get total
 			$totals = array();
@@ -162,51 +98,164 @@ if ( !class_exists( 'MP_Gateway_myCRED' ) ) {
 			if ( ( $tax_price = $mp->tax_price() ) !== false ) {
 				$total = $total + $tax_price;
 			}
+			
+			$settings = get_option( 'mp_settings' );
+			if ( $this->use_exchange() )
+				return $this->mycred->apply_exchange_rate( $total, $settings['gateways']['mycred']['exchange'] );
+			else
+				return $this->mycred->number( $total );
+		}
 
-			$balance = $mycred->get_users_cred( $user_id );
-			if ( $balance <= 0 ) {
+		/**
+		 * Return fields you need to add to the payment screen, like your credit card info fields
+		 *
+		 * @param array $cart. Contains the cart contents for the current blog, global cart if $mp->global_cart is true
+		 * @param array $shipping_info. Contains shipping info and email in case you need it
+		 * @since 1.1
+		 * @version 1.1
+		 */
+		function payment_form( $cart, $shipping_info ) {
+			global $mp;
+			
+			$settings = get_option( 'mp_settings' );
+			
+			if ( !is_user_logged_in() ) {
+				$message = str_replace( '%login_url_here%', wp_login_url( mp_checkout_step_url( 'checkout' ) ), $settings['gateways']['mycred']['visitors'] );
+				$message = $this->mycred->template_tags_general( $message );
+				return '<div id="mp-mycred-balance">' . $message . '</div>';
+			}
+			
+			$balance = $this->mycred->get_users_cred( get_current_user_id() );
+			$total = $this->get_cart_total( $cart );
+			
+			// Low balance
+			if ( $balance-$total < 0 ) {
+				$message = $this->mycred->template_tags_user( $settings['gateways']['mycred']['lowfunds'], false, wp_get_current_user() );
+				$instructions = '<div id="mp-mycred-balance">' . $message . '</div>';
+				$red = ' style="color: red;"';
+			}
+			else {
+				$instructions = $this->mycred->template_tags_general( $settings['gateways']['mycred']['instructions'] );
+				$red = '';
+			}
+			
+			// Return Cost
+			return '
+<div id="mp-mycred-balance">' . $instructions . '</div>
+<div id="mp-mycred-cost">
+<table style="width:100%;">
+	<tr>
+		<td class="info">' . __( 'Current Balance', 'mycred' ) . '</td>
+		<td class="amount">' . $this->mycred->format_creds( $balance ) . '</td>
+	</tr>
+	<tr>
+		<td class="info">' . __( 'Total Cost', 'mycred' ) . '</td>
+		<td class="amount">' . $this->mycred->format_creds( $total ) . '</td>
+	</tr>
+	<tr>
+		<td class="info">' . __( 'Balance After Purchase', 'mycred' ) . '</td>
+		<td class="amount"' . $red . '>' . $this->mycred->format_creds( $balance-$total ) . '</td>
+	</tr>
+</table>
+</div>';
+		}
+
+		/**
+		 * Return the chosen payment details here for final confirmation. You probably don't need
+		 * to post anything in the form as it should be in your $_SESSION var already.
+		 *
+		 * @param array $cart. Contains the cart contents for the current blog, global cart if $mp->global_cart is true
+		 * @param array $shipping_info. Contains shipping info and email in case you need it
+		 * @since 1.1
+		 * @version 1.1
+		 */
+		function confirm_payment_form( $cart, $shipping_info ) {
+			global $mp;
+
+			$settings = get_option( 'mp_settings' );
+			$user_id = get_current_user_id();
+			$balance = $this->mycred->get_users_cred( get_current_user_id() );
+			$total = $this->get_cart_total( $cart );
+			
+			$table = '<table class="mycred-cart-cost"><thead><tr><th>' . __( 'Payment', 'mycred' ) . '</th></tr></thead>';
+			if ( $balance-$total < 0 ) {
+				$message = $this->mycred->template_tags_user( $settings['gateways']['mycred']['lowfunds'], false, wp_get_current_user() );
+				$table .= '<tr><td id="mp-mycred-cost" style="color: red; font-weight: bold;"><p>' . $message . ' ' . sprintf( __( '<a href="%s">Go Back</a>', 'mycred' ), mp_checkout_step_url( 'checkout' ) ) . '</td></tr>';
+			}
+			else
+				$table .= '<tr><td id="mp-mycred-cost" class="mycred-ok">' . $this->mycred->format_creds( $total ) . ' ' . __( 'will be deducted from your account.', 'mycred' ) . '</td></tr>';
+			
+			return $table . '</table>';
+		}
+		
+		function process_payment_form( $cart, $shipping_info ) { }
+
+		/**
+		 * Use this to do the final payment. Create the order then process the payment. If
+		 * you know the payment is successful right away go ahead and change the order status
+		 * as well.
+		 * Call $mp->cart_checkout_error($msg, $context); to handle errors. If no errors
+		 * it will redirect to the next step.
+		 *
+		 * @param array $cart. Contains the cart contents for the current blog, global cart if $mp->global_cart is true
+		 * @param array $shipping_info. Contains shipping info and email in case you need it
+		 * @since 1.1
+		 * @version 1.2
+		 */
+		function process_payment( $cart, $shipping_info ) {
+			global $mp;
+			
+			$settings = get_option('mp_settings');
+			$user_id = get_current_user_id();
+			$insolvent = $this->mycred->template_tags_user( $settings['gateways']['mycred']['lowfunds'], false, wp_get_current_user() );
+			$timestamp = time();
+
+			// This gateway requires buyer to be logged in
+			if ( !is_user_logged_in() ) {
+				$message = str_replace( '%login_url_here%', wp_login_url( mp_checkout_step_url( 'checkout' ) ), $settings['gateways']['mycred']['visitors'] );
+				$mp->cart_checkout_error( $this->mycred->template_tags_general( $message ) );
+			}
+
+			// Make sure current user is not excluded from using myCRED
+			if ( $this->mycred->exclude_user( $user_id ) )
 				$mp->cart_checkout_error(
-					sprintf( __( 'Insufficient Funds Please select a different payment method. <a href="%s">Go Back</a>', 'mycred' ), mp_checkout_step_url( 'checkout' ) )
+					sprintf( __( 'Sorry, but you can not use this gateway as your account is excluded. Please <a href="%s">select a different payment method</a>.', 'mycred' ), mp_checkout_step_url( 'checkout' ) )
+				);
+
+			// Get users balance
+			$balance = $this->mycred->get_users_cred( $user_id );
+			$total = $this->get_cart_total( $cart );
+			
+			// Low balance or Insolvent
+			if ( $balance <= 0 || $balance-$total < 0 ) {
+				$mp->cart_checkout_error(
+					$insolvent . ' ' . sprintf( __( '<a href="%s">Go Back</a>', 'mycred' ), mp_checkout_step_url( 'checkout' ) )
 				);
 				return;
 			}
-			if ( $this->use_exchange() )
-				$total = $mycred->apply_exchange_rate( $mycred->number( $total ), $settings['gateways']['mycred']['exchange'] );
 			
-			// Check if there is enough to fund this
-			if ( $balance-$total >= 0 ) {
-				// Create MarketPress order
-				$order_id = $mp->generate_order_id();
-				$payment_info['gateway_public_name'] = $this->public_name;
-				$payment_info['gateway_private_name'] = $this->admin_name;
-				$payment_info['status'][$timestamp] = __( 'Paid', 'mycred' );
-				$payment_info['total'] = $total;
-				$payment_info['currency'] = $settings['currency'];
-				$payment_info['method'] = __( 'myCRED', 'mycred' );
-				$payment_info['transaction_id'] = $order_id;
-				$paid = true;
-				$result = $mp->create_order( $order_id, $cart, $shipping_info, $payment_info, $paid );
+			// Create MarketPress order
+			$order_id = $mp->generate_order_id();
+			$payment_info['gateway_public_name'] = $this->public_name;
+			$payment_info['gateway_private_name'] = $this->admin_name;
+			$payment_info['status'][$timestamp] = __( 'Paid', 'mycred' );
+			$payment_info['total'] = $total;
+			$payment_info['currency'] = $settings['currency'];
+			$payment_info['method'] = __( 'myCRED', 'mycred' );
+			$payment_info['transaction_id'] = $order_id;
+			$paid = true;
+			$result = $mp->create_order( $order_id, $cart, $shipping_info, $payment_info, $paid );
 				
-				$order = get_page_by_title( $result, 'OBJECT', 'mp_order' );
-				// Deduct cost
-				$mycred->add_creds(
-					'marketpress_payment',
-					$user_id,
-					0-$total,
-					$settings['gateways']['mycred']['log_template'],
-					$order->ID,
-					array( 'ref_type' => 'post' )
-				);
-				
-				
-				
-			}
-			// Insuffient Funds
-			else {
-				$mp->cart_checkout_error(
-					sprintf( __( 'Insufficient Funds Please select a different payment method. <a href="%s">Go Back</a>', 'mycred' ), mp_checkout_step_url( 'checkout' ) )
-				);
-			}
+			$order = get_page_by_title( $result, 'OBJECT', 'mp_order' );
+			// Deduct cost
+			$this->mycred->add_creds(
+				'marketpress_payment',
+				$user_id,
+				0-$total,
+				$settings['gateways']['mycred']['log_template'],
+				$order->ID,
+				array( 'ref_type' => 'post' )
+			);
 		}
 		
 		function order_confirmation( $order ) { }
@@ -233,7 +282,7 @@ if ( !class_exists( 'MP_Gateway_myCRED' ) ) {
 		 * Return any html you want to show on the confirmation screen after checkout. This
 		 * should be a payment details box and message.
 		 * @since 1.1
-		 * @version 1.0
+		 * @version 1.1
 		 */
 		function order_confirmation_msg( $content, $order ) {
 			global $mp;
@@ -245,14 +294,14 @@ if ( !class_exists( 'MP_Gateway_myCRED' ) ) {
 			return $content . str_replace(
 				'TOTAL',
 				$mp->format_currency( $order->mp_payment_info['currency'], $order->mp_payment_info['total'] ),
-				$mycred->template_tags_user( $settings['gateways']['mycred']['confirmation'], $user_id )
+				$mycred->template_tags_user( $settings['gateways']['mycred']['confirmation'], false, wp_get_current_user() )
 			);
 		}
 
 		/**
 		 * myCRED Gateway Settings
 		 * @since 1.1
-		 * @version 1.0
+		 * @version 1.1
 		 */
 		function gateway_settings_box( $settings ) {
 			global $mp;
@@ -278,6 +327,12 @@ if ( !class_exists( 'MP_Gateway_myCRED' ) ) {
 			
 			if ( !isset( $settings['gateways']['mycred']['confirmation'] ) )
 				$settings['gateways']['mycred']['confirmation'] = 'TOTAL amount has been deducted from your account. Your current balance is: %balance_f%';
+			
+			if ( !isset( $settings['gateways']['mycred']['lowfunds'] ) )
+				$settings['gateways']['mycred']['lowfunds'] = 'Insufficient funds. Please select another form of payment. Your current balance is: %balance_f%';
+				
+			if ( !isset( $settings['gateways']['mycred']['visitors'] ) )
+				$settings['gateways']['mycred']['visitors'] = 'You must be logged in to pay with %_plural%. Please <a href="%login_url_here%">login</a>.';
 
 			if ( !isset( $settings['gateways']['mycred']['email'] ) )
 				$settings['gateways']['mycred']['email'] = $settings['email']['new_order_txt']; ?>
@@ -324,6 +379,22 @@ if ( !class_exists( 'MP_Gateway_myCRED' ) ) {
 <?php		endif; ?>
 
 			<tr>
+				<th scope="row"><label for="mycred-lowfunds"><?php _e( 'Insufficient Funds', 'mycred' ); ?></label></th>
+				<td>
+					<span class="description"><?php _e( 'Message to show when the user can not use this gateway.', 'mycred' ); ?></span>
+					<p><input type="text" name="mp[gateways][mycred][lowfunds]" id="mycred-lowfunds" style="width: 100%;" value="<?php echo esc_attr( $settings['gateways']['mycred']['lowfunds'] ); ?>"><br />
+					<span class="description"><?php _e( 'Available template tags are: General.', 'mycred' ); ?></span></p>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><label for="mycred-visitors"><?php _e( 'Visitors', 'mycred' ); ?></label></th>
+				<td>
+					<span class="description"><?php _e( 'Message to show to buyers that are not logged in.', 'mycred' ); ?></span>
+					<p><input type="text" name="mp[gateways][mycred][visitors]" id="mycred-visitors" style="width: 100%;" value="<?php echo esc_attr( $settings['gateways']['mycred']['visitors'] ); ?>"><br />
+					<span class="description"><?php _e( 'Available template tags are: General.', 'mycred' ); ?></span></p>
+				</td>
+			</tr>
+			<tr>
 				<th scope="row"><label for="mycred-instructions"><?php _e( 'User Instructions', 'mycred' ); ?></label></th>
 				<td>
 					<span class="description"><?php _e( 'Information to show users before payment.', 'mycred' ); ?></span>
@@ -356,7 +427,7 @@ if ( !class_exists( 'MP_Gateway_myCRED' ) ) {
 		/**
 		 * Filter Gateway Settings
 		 * @since 1.1
-		 * @version 1.0
+		 * @version 1.1
 		 */
 		function process_gateway_settings( $settings ) {
 			// Name (no html)
@@ -378,11 +449,10 @@ if ( !class_exists( 'MP_Gateway_myCRED' ) ) {
 			else
 				$settings['gateways']['mycred']['exchange'] = 1;
 			
-			// Filter Instruction & Confirmation (if needed)
-			if ( !current_user_can( 'unfiltered_html' ) ) {
-				$settings['gateways']['mycred']['instructions'] = wp_filter_post_kses( $settings['gateways']['mycred']['instructions'] );
-				$settings['gateways']['mycred']['confirmation'] = wp_filter_post_kses( $settings['gateways']['mycred']['confirmation'] );
-			}
+			$settings['gateways']['mycred']['lowfunds'] = stripslashes( wp_filter_post_kses( $settings['gateways']['mycred']['lowfunds'] ) );
+			$settings['gateways']['mycred']['visitors'] = stripslashes( wp_filter_post_kses( $settings['gateways']['mycred']['visitors'] ) );
+			$settings['gateways']['mycred']['instructions'] = stripslashes( wp_filter_post_kses( $settings['gateways']['mycred']['instructions'] ) );
+			$settings['gateways']['mycred']['confirmation'] = stripslashes( wp_filter_post_kses( $settings['gateways']['mycred']['confirmation'] ) );
 
 			// Email (no html)
 			$settings['gateways']['mycred']['email'] = stripslashes( wp_filter_nohtml_kses( $settings['gateways']['mycred']['email'] ) );
