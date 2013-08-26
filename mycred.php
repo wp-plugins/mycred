21126553107b953349fd714c2513946116176b23
designbymerovingi <designbymerovingi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-26 09:38:54 +0000

    1.2.2 Prep
    git-svn-id: https://plugins.svn.wordpress.org/mycred/trunk@762336 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mycred.php
+++ mycred.php
@@ -3,7 +3,7 @@
 Plugin Name: myCRED
 Plugin URI: http://mycred.me
 Description: <strong>my</strong>CRED is an adaptive points management system for WordPress powered websites, giving you full control on how points are gained, used, traded, managed, logged or presented.
-Version: 1.2.2Beta3
+Version: 1.2.2
 Tags: points, tokens, credit, management, reward, charge
 Author: Gabriel S Merovingi
 Author URI: http://www.merovingi.com
@@ -15,7 +15,7 @@ Domain Path: /lang
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
-define( 'myCRED_VERSION',      '1.2.2Beta3' );
+define( 'myCRED_VERSION',      '1.2.2' );
 define( 'myCRED_SLUG',         'mycred' );
 define( 'myCRED_NAME',         '<strong>my</strong>CRED' );
 
